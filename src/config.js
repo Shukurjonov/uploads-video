888b08499c5c6e715657b309eb3452341879dcc1
Shukurjonov <asliddinshukurjonov@gmail.com>
2021-06-30 03:49:29 +0500

    change config.js settings

--- src/config.js
+++ src/config.js
@@ -1,22 +1,7 @@
-const { networkInterfaces } = require('os')
+const ip = require('./lib/getIp')
 
-const nets = networkInterfaces()
-const results = {}
-
-for (const name of Object.keys(nets)) {
-    for (const net of nets[name]) {
-        if (net.family === 'IPv4' && !net.internal) {
-            if (!results[name]) {
-                results[name] = [];
-            }
-            results[name].push(net.address);
-        }
-    }
-}
-
-
-const host = results["Беспроводная сеть"][0]
-
+const host = ip({internal: false})
 const PORT = process.env.PORT || 5000
 
 module.exports = {host, PORT}
+
